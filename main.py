894e2afbc16c8e09ff477118652691c7d2aa531f
catsmine <47918176+catsmine@users.noreply.github.com>
2022-10-15 17:53:45 +0200

    Add graph

--- main.py
+++ main.py
@@ -11,9 +11,31 @@ import pandas
 # In[ ]:
 
 
-st.title ('Our first Streamlit App')
-st.subheader('Introducing Streamlit in Automate Everything')
-st.write('''This is our first WebApp.
-            Enjoy it!
-         ''')
+data = {
+  'Series_1':[1, 3, 4, 5, 7],
+  'Series 2':[10, 30, 40, 100, 250]
+}
+
+df = pandas.DataFrame(data)
+
+st.title('Our First Streamlit App')
+st.subheader('Introducing Streamlit in Automate Everything with Python')
+st.write('''This is our first Web App.
+Enjoy it!
+''')
+st.write(df)
+st.line_chart(df)
+st.area_chart(df)
+
+myslider = st.slider('Celsius')
+st.write(myslider, 'in Fahrenheit is', myslider * 9/5 + 32)
+
+#to run this, download file as main.py, change directory to current folder in the anaconda prompt, 
+#then type streamlit run main.py
+
+
+# In[ ]:
+
+
+
 
