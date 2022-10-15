b0fdbbdbba0e5d9e7c076674007748e2ee381462
catsmine <47918176+catsmine@users.noreply.github.com>
2022-10-15 17:39:08 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import streamlit as st
+import pandas
+
+
+# In[ ]:
+
+
+st.title ('Our first Streamlit App')
+st.subheader('Introducing Streamlit in Automate Everything')
+st.write('''This is our first WebApp.
+            Enjoy it!
+         ''')
+
